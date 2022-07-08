d8bdc15a292c415057b63a09a8a035062e4c829c
Ahmetmete0 <58399721+Ahmetmete0@users.noreply.github.com>
2022-07-08 15:48:53 +0300

    Transform

--- src/parser/src/local_cartesian.cpp
+++ src/parser/src/local_cartesian.cpp
@@ -7,12 +7,15 @@
 #include <GeographicLib/LocalCartesian.hpp>
 #include <fstream>
 
+#include <stdlib.h>
+#include <string>
 #include <vector>
 #include <bits/stdc++.h>
 
 
 
 #include <nav_msgs/msg/path.hpp>
+#include <geometry_msgs/msg/PoseStamped.hpp>
 #include "rclcpp/rclcpp.hpp"
 
 #include "kml/dom.h"  // The KML DOM header.
@@ -20,6 +23,12 @@
  
 using namespace std;
 using namespace GeographicLib;
+
+vector<string> v;
+vector<string> longitude_array;
+vector<string> latitude_array;
+vector<string> altitude_array;
+
 
 class MinimalPublisher : public rclcpp::Node
 {
@@ -54,10 +63,7 @@ class MinimalPublisher : public rclcpp::Node
  
 int main(int argc, char * argv[]) {
 
-  vector<string> v;
-  vector<string> longitude_array;
-  vector<string> latitude_array;
-  vector<string> altitude_array;
+
   ifstream dosyaOku("../catkin_ws2/src/parser/doc/coordinates.txt");
   string satir = "";
   int counter_1 = 0;
@@ -93,7 +99,7 @@ int main(int argc, char * argv[]) {
 
       for (size_t i = 0; i < latitude_array.size(); i++){
         //cout << v[i] << endl;
-        cout << longitude_array[i] << endl;
+        //cout << longitude_array[i] << endl;
         //cout << latitude_array[i] << endl;
         //cout << altitude_array[i] << endl;
 
@@ -116,22 +122,36 @@ int main(int argc, char * argv[]) {
   try {
     Geocentric earth(Constants::WGS84_a(), Constants::WGS84_f());
     // Alternatively: const Geocentric& earth = Geocentric::WGS84();
-    const double lat0 = 28.98113386763581, lon0 = 41.02535119363602; // Paris
-    LocalCartesian proj(lat0, lon0, 1.81700869684152, earth);
+    //const double lat0 = 28.98113386763581, lon0 = 41.02535119363602; // Paris
+    double origin_long, origin_lat, origin_alt;
+
+    origin_lat = stod(latitude_array[0]);
+    origin_long = stod(longitude_array[0]);
+    origin_alt = stod(altitude_array[0]);
+    
+    LocalCartesian proj(origin_lat, origin_long, origin_alt, earth);
     {
       // Sample forward calculation
-      double lat = 50.9, lon = 1.8, h = 0; // Calais
-      double x, y, z;
-      proj.Forward(lat, lon, h, x, y, z);
-      cout << x << " " << y << " " << z << "\n";
+      // From Geodetic to local cartesian
+
+      for (size_t i = 0; i < latitude_array.size(); i++){
+        //cout << v[i] << endl;
+        //cout << longitude_array[i] << endl;
+        //cout << latitude_array[i] << endl;
+        //cout << altitude_array[i] << endl;
+        double x, y, z;
+        proj.Forward(stod(latitude_array[i]), stod(longitude_array[i]), stod(altitude_array[i]), x, y, z);
+        cout << x << " " << y << " " << z << "\n";    
+      }
+      //double lat = 50.9, lon = 1.8, h = 0; // Calais
     }
-    {
+    /*{
       // Sample reverse calculation
       double x = -38e3, y = 230e3, z = -4e3;
       double lat, lon, h;
       proj.Reverse(x, y, z, lat, lon, h);
       cout << lat << " " << lon << " " << h << "\n";
-    }
+    }**/
 
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<MinimalPublisher>());
